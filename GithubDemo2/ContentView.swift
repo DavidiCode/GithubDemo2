2476cbee62c16e981b1d436ce86740628e1c2ab0
DavidiCode <akbar.icode@gmail.com>
2024-02-28 22:57:49 +0500

    Second commit

--- GithubDemo2/ContentView.swift
+++ GithubDemo2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Welcome to SwiftUI")
+            Text("Welcome to SwiftUI !!!")
         }
         .padding()
     }
