6e57c295f9aa4e509ce15bd5265d7afb2df09a7c
DavidiCode <akbar.icode@gmail.com>
2024-02-28 22:54:47 +0500

    Initial Commit

---
+++ GithubDemo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubDemo2
+//
+//  Created by Akbar Ibodulloyev on 28/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to SwiftUI")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubDemo2/GithubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo2App.swift
+//  GithubDemo2
+//
+//  Created by Akbar Ibodulloyev on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
